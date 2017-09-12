fb84e59533fc33a4dceea198653600dc00b375b1
semitro <semitro_8@mail.ru>
2017-09-12 21:22:34 +0300

    master

---
+++ lab1/hitTheArea.php
@@ -0,0 +1,6 @@
+<?php
+$x=$_POST['X'];
+echo $x;
+//echo "fsf";
+//echo "xui";
+?>
---
+++ lab1/validateAreaForm.js
@@ -0,0 +1,5 @@
+function validate(){
+	alert("hello world");
+	var x =document.getElementById("x").value;
+	alert(x);
+}
