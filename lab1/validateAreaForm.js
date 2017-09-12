9c87b9b67ee9b3e76c5ebeb1492b52d30e1595a7
semitro <semitro_8@mail.ru>
2017-09-12 23:27:42 +0300

    There is an emphasize

--- lab1/validateAreaForm.js
+++ lab1/validateAreaForm.js
@@ -1,5 +1,17 @@
 function validate(){
-	alert("hello world");
-	var x =document.getElementById("x").value;
-	alert(x);
+	var x = document.getElementById("x").value;
+	var hidden = document.getElementById("whichButton").value;
+	alert(hidden);
+}
+
+function yButtonPress(whichButton){
+	//set value of the hidden mergin
+	document.getElementById('whichButton').value=whichButton;
+	var elements = document.getElementsByClassName("butt");
+    for (var butt in elements ) {
+		// butt.style.backgroundColor = "red";
+		if(elements[butt].value == whichButton )
+
+		elements[butt].style.backgroundColor = "#736144";
+    }
 }
