fab725d30a5c19726036c2707b83783c9307c7b1
semitro <semitro_8@mail.ru>
2017-09-12 23:58:28 +0300

    The emphasize was improved

--- lab1/validateAreaForm.js
+++ lab1/validateAreaForm.js
@@ -1,17 +1,29 @@
 function validate(){
-	var x = document.getElementById("x").value;
+	var x = document
+	.getElementById("x").value;
 	var hidden = document.getElementById("whichButton").value;
 	alert(hidden);
 }
 
+var styleColor;
+var alreadyBeenHere = false;
 function yButtonPress(whichButton){
-	//set value of the hidden mergin
-	document.getElementById('whichButton').value=whichButton;
-	var elements = document.getElementsByClassName("butt");
-    for (var butt in elements ) {
-		// butt.style.backgroundColor = "red";
-		if(elements[butt].value == whichButton )
 
-		elements[butt].style.backgroundColor = "#736144";
-    }
+	//set up value of the hidden mergin
+	document.getElementById('whichButton').value=whichButton;
+ 
+	var elements = document.getElementsByClassName("butt");
+
+	// We need to remember the buttonses true color
+	if(!alreadyBeenHere){
+		styleColor = elements[0].style.backgroundColor;
+		alreadyBeenHere = true;
+	}
+
+
+    for (var butt in elements )
+		if(elements[butt].value == whichButton )
+			elements[butt].style.backgroundColor = "#736144";
+		else
+			elements[butt].style.backgroundColor = styleColor;
 }
