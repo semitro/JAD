ae39995a67594a0633a03d4d126104e312e5dfbb
semitro <semitro_8@mail.ru>
2017-09-13 00:47:22 +0300

    Validation added

--- lab1/validateAreaForm.js
+++ lab1/validateAreaForm.js
@@ -1,10 +1,41 @@
+
+
 function validate(){
-	var x = document
-	.getElementById("x").value;
-	var hidden = document.getElementById("whichButton").value;
-	alert(hidden);
+if(isEverythingOk())
+	document.getElementById("mainForm").submit();
 }
 
+function isEverythingOk(){
+
+	if(document.getElementById("whichButton").value == "default"){
+		alert("Select Y please");
+		return false;
+	}
+	var r = document.getElementById("R").value;
+	var x = document.getElementById("x").value;
+	// if(!/[0-9]*([\.,][0-9]*)?/.test(x) 
+	// 	|| 
+	//    !/[0-9]*([\.,][0-9]*)?/.test(r))
+	// 	return false;
+
+	if(!isNumber(r) || !isNumber(x)){
+		alert("R and X must be numbers");
+		return false;
+	}
+
+	if(r < 2 || r > 5) { alert("R must be in [2,5]"); return false; }
+	if(x<-5  || x > 3) { alert("X must be in [-5,3]"); return false; }
+
+	return true;
+}
+
+function isNumber(n){
+	return parseFloat(n) == n;
+ }
+
+/**
+* A function to hightlight a button last pressed
+**/
 var styleColor;
 var alreadyBeenHere = false;
 function yButtonPress(whichButton){
