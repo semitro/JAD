1d7cdc8ca16f57316db172c9ee065ad5a147a340
trinistr <trinistr@gmail.com>
2017-09-13 16:18:59 +0300

    Добавлен скрипт для AJAX

---
+++ lab1/ajax.js
@@ -0,0 +1,21 @@
+function ajax(url, callback){
+	var ajaxRequest;
+	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("AJAX не работает!");return false;}}}
+	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){callback(ajaxRequest.responseText, ajaxRequest.status);}}}
+	ajaxRequest.open('GET',url,true);
+	ajaxRequest.send(null);
+}
+
+function ajaxpost(url, body, callback, encoding) {
+	var ajaxRequest;
+	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("AJAX не работает!");return false;}}}
+	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){callback(ajaxRequest.responseText, ajaxRequest.status);}}}
+	ajaxRequest.open('POST',url,true);
+	if(encoding!=undefined){
+		ajaxRequest.setRequestHeader('Content-Type', encoding);
+	}
+	else {
+		ajaxRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+	}
+	ajaxRequest.send(body);
+}
