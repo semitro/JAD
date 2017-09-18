8444ba62325de86b6d0011280d8436105256ad4a
semitro <semitro_8@mail.ru>
2017-09-19 00:15:37 +0300

    fixed small big with selected button color

---
+++ lab1/deploy.sh
@@ -0,0 +1,1 @@
+scp -P 2222 * s225065@se.ifmo.ru:~/public_html
--- lab1/validateAreaForm.js
+++ lab1/validateAreaForm.js
@@ -1,9 +1,5 @@
-ajaxpost("hitTheArea.php","checkMath=true", function(response, code) {
-	if(response == "false") {
-		 document.getElementById('results').innerHTML += 
-			"<tr><td colspan='6'>ВНИМАНИЕ! ТОЧНЫЕ РЕЗУЛЬТАТЫ НЕ ГАРАНТИРОВАНЫ! (Нужен модуль BCMath)</td></tr>";
-	}
-}
+
+
 
 function validate(){
 	if(isEverythingOk()){
@@ -61,16 +57,20 @@ function isNumber(n){
 * A function to hightlight a button last pressed
 **/
 var lastButton=null;
+var lastColor=null;
 function yButtonPress(whichButton){
 	if(lastButton == whichButton)
 		return;
+	lastColor=whichButton.style.color
 	document.forms[0].elements.Y.value = whichButton.value;
-	whichButton.style.backgroundColor = "#736144";
+	whichButton.style.backgroundColor = "#C36144";
 	if(lastButton) {
-		lastButton.style.backgroundColor = "#B94040";
+		lastButton.style.backgroundColor = lastColor;
 	}
 	lastButton = whichButton;
 }
+
+
 
 
 function ajaxpost(url, body, callback, encoding) {
@@ -97,4 +97,3 @@ function ajaxCallback(response, code) {
 		errorel.style.color = "yellow";
 	}
 }
-
