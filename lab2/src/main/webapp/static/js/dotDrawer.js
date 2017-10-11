725e9b211b6d4d307cbe786deedfc88ba925498a
semitro <semitro_8@mail.ru>
2017-10-11 12:52:18 +0300

    Точки на графике пересчитываются в математическое значение

--- lab2/src/main/webapp/static/js/dotDrawer.js
+++ lab2/src/main/webapp/static/js/dotDrawer.js
@@ -5,16 +5,39 @@
 $('#plotCanvas').bind('click',onPlotClick);
 
 function onPlotClick(inf){
-    drawPoint(inf.offsetX,inf.offsetY);
+    // isNumber is defined in validateAreaForm.js
+    var err = isEverythingOk()
+    if( err == "OK"){
+        drawPoint(inf.offsetX,inf.offsetY);
+        // Пересчёт в координаты математической модели
+        console.log(calculateX(inf.offsetX,document.forms[0].R.value));
+        console.log(calculateY(inf.offsetY,document.forms[0].R.value));
+
+    }
+    else
+        toastr.error("Ммвыв",err);
+
 }
 
 function drawPoint(x, y) {
-    console.log(x);
-    console.log(y);
     var c = document.getElementById("plotCanvas");
     var ctx = c.getContext("2d");
     ctx.beginPath();
     ctx.arc(x,y,2,0,2*Math.PI);
     ctx.arc(x,y,4,0,2*Math.PI);
     ctx.stroke();
+}
+// Внимание! Всё привязано к конкретному изображению.
+// Эмпирически - для точки (0;0) offsetX = 112 offsetY = 111
+//               для точки (R;0) offsetX = 190 offsetY = 111
+// Значит, расположение точки в воображаемом графике:
+// R * ( (offsetXR - offsetZero) / (clickOffsetX - offsetZero) )
+var ZeroX = 112, ZeroY = 111,
+    offsetR = 190;
+
+function calculateX(clickOffsetX, currentR) {
+    return currentR * ( (clickOffsetX-ZeroX)/(offsetR - ZeroX));
+}
+function calculateY(clickOffsetY, currentR) {
+    return currentR * ( (clickOffsetY-ZeroY)/(ZeroY - offsetR));
 }
--- lab2/target/semitro-lab2-1.0-SNAPSHOT/static/js/dotDrawer.js
+++ lab2/target/semitro-lab2-1.0-SNAPSHOT/static/js/dotDrawer.js
@@ -2,22 +2,42 @@
  * Created by semitro on 09.10.17.
  */
 
-$('.plot').bind('click',onPlotClick);
+$('#plotCanvas').bind('click',onPlotClick);
 
 function onPlotClick(inf){
-
-    drawPoint(inf.pageX - document.getElementById("plotCanvas").offsetLeft,
-        inf.pageY - document.getElementById("plotCanvas").offsetTop);
-}
-
- function drawPoint(x, y) {
-     console.log(x);
-     console.log(y);
-     var c = document.getElementById("plotCanvas");
-     var ctx = c.getContext("2d");
-     ctx.beginPath();
-     ctx.arc(95,50,2,0,2*Math.PI);
-     ctx.arc(95,50,4,0,2*Math.PI);
-     ctx.stroke();
+    // isNumber is defined in validateAreaForm.js
+    var err = isEverythingOk()
+    if( err == "OK"){
+        drawPoint(inf.offsetX,inf.offsetY);
+        // Пересчёт в координаты математической модели
+        console.log(calculateX(inf.offsetX,document.forms[0].R.value));
+        console.log(calculateY(inf.offsetY,document.forms[0].R.value));
 
     }
+    else
+        toastr.error("Ммвыв",err);
+
+}
+
+function drawPoint(x, y) {
+    var c = document.getElementById("plotCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(x,y,2,0,2*Math.PI);
+    ctx.arc(x,y,4,0,2*Math.PI);
+    ctx.stroke();
+}
+// Внимание! Всё привязано к конкретному изображению.
+// Эмпирически - для точки (0;0) offsetX = 112 offsetY = 111
+//               для точки (R;0) offsetX = 190 offsetY = 111
+// Значит, расположение точки в воображаемом графике:
+// R * ( (offsetXR - offsetZero) / (clickOffsetX - offsetZero) )
+var ZeroX = 112, ZeroY = 111,
+    offsetR = 190;
+
+function calculateX(clickOffsetX, currentR) {
+    return currentR * ( (clickOffsetX-ZeroX)/(offsetR - ZeroX));
+}
+function calculateY(clickOffsetY, currentR) {
+    return currentR * ( (clickOffsetY-ZeroY)/(ZeroY - offsetR));
+}
