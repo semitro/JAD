243ffe6e4b50eff6e197ba3c4d4575eaf84a458f
trinistr <trinistr@gmail.com>
2017-10-11 14:17:57 +0300

    Изменён AreaChecker

--- lab2/src/main/java/logic/AreaChecker.java
+++ lab2/src/main/java/logic/AreaChecker.java
@@ -10,23 +10,23 @@ public class AreaChecker implements AreaCheckerInterface {
        BigDecimal big_x = (BigDecimal)x;
        BigDecimal big_y = (BigDecimal)y;
        BigDecimal big_R = (BigDecimal)R;
-
-       // Если y >= 0 and y <= R
-       if(        big_y.compareTo(BigDecimal.ZERO) >= 0
-               && big_y.compareTo(big_R) <= 0){
-
+       // Если y >= 0 
+       if(big_y.compareTo(BigDecimal.ZERO) >= 0) {
            // Первая четверть
            if(big_x.compareTo(BigDecimal.ZERO) >= 0
               &&big_x.compareTo(big_R) <= 0)
                return true;
-           // Вторая четверть
+           // Вторая четверть или не попали
            return false;
        }
-
        // Третья четверть
-       if(big_x.compareTo(BigDecimal.ZERO) < 0){
-           // Допилить на свежую голову
+       if(big_x.compareTo(BigDecimal.ZERO) <= 0){
+		   if(big_y.compareTo(big_x.negate().subtract(big_R))>=0)
+			   return true;
+			return false;
        }
-       return true;
+       if(big_x.pow(2).add(big_y.pow(2)).compareTo(big_R.divide(BigDecimal(2)).pow(2)) <= 0)
+			return true;
+       return false;
     }
 }
