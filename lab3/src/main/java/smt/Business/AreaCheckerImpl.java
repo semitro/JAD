170d6a3b7ae7ef1473dff3c5d9f433ae16274967
semitro <semitro_8@mail.ru>
2017-11-18 15:03:47 +0300

    Попадание в область, немаскируемое перенаправление

--- lab3/src/main/java/smt/Business/AreaCheckerImpl.java
+++ lab3/src/main/java/smt/Business/AreaCheckerImpl.java
@@ -14,20 +14,24 @@ public class AreaCheckerImpl implements AreaChecker {
          * less than, equal to, or greater than {@code val}.
          */
         // 1'st quoter
+
         if(x.compareTo(BigDecimal.ZERO) >= 0 && y.compareTo(BigDecimal.ZERO) >= 0){
-           // return x.pow(2).plus( y.pow(2) ).
+            // x^2 + y^2 not greater than (r/2)^2
+            return x.pow(2).add( y.pow(2) ).compareTo( r.divide(BigDecimal.valueOf(2.)).pow(2)) != 1 ;
         }
         else // 2'st quoter
         if(x.compareTo(BigDecimal.ZERO) < 0 && y.compareTo(BigDecimal.ZERO) >=0){
-
+            // x >= -r && y <= r/2
+            return x.compareTo(r.negate()) >= 0 && y.compareTo(r.divide(BigDecimal.valueOf(2.))) <= 0;
         }
         else // 3'st quoter
         if(x.compareTo(BigDecimal.ZERO) < 0 && y.compareTo(BigDecimal.ZERO) < 0){
-
+            // y >= -r + x
+            return y.compareTo(r.negate().add(x)) >= 0;
         }
         else // 4'st quoter
         if(x.compareTo(BigDecimal.ZERO) >= 0 && y.compareTo(BigDecimal.ZERO) <0){
-
+            // always false, nothing to check
         }
         return false;
     }
