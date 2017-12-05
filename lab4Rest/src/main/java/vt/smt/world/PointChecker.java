acde5e26ad598325a184782b1a2805b8c8a30bc9
semitro <semitro_8@mail.ru>
2017-12-06 02:22:58 +0300

    Работает POST-запрос через JSON

--- lab4Rest/src/main/java/vt/smt/world/PointChecker.java
+++ lab4Rest/src/main/java/vt/smt/world/PointChecker.java
@@ -2,10 +2,14 @@ package vt.smt.world;
 
 import vt.smt.Business.AreaChecker;
 import vt.smt.Business.DataBaseInteraction;
+import vt.smt.Business.Point;
 
 import javax.ejb.Stateless;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Stateless
 @Path("/points")
@@ -20,12 +24,16 @@ public class PointChecker {
     public String sayHello(){
         return "Hello";
     }
-    //@javax.ws.rs.POST
-//    @javax.ws.rs.GET
-//    public Point doesItHit(Point p){
-//        Point ans = new Point(p);
+    @javax.ws.rs.POST
+    @Path("/hit")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Point doesItHit(Point p){
+//        Point ans = new Point();
 //        ans.setHit(true);
-//        return ans;
-//       // ans.setHit(areaCheker.doesPointHit(p));
-//    }
+        p.setHit(true);
+        return p;
+       // return Response.ok(json, MediaType.APPLICATION_JSON).build();
+       // ans.setHit(areaCheker.doesPointHit(p));
+    }
 }
